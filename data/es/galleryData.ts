d5ea97bcaa0c290ccb67c0c45f6f420f858bddff
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-10-20 23:45:55 +0700

    Create galleryData.ts

---
+++ data/es/galleryData.ts
@@ -0,0 +1,69 @@
+import type { Project_g } from '~/types/data'
+
+export let galleryData: Project_g[] = [
+  {
+    type: 'indonesia',
+    title: 'Aa - Universal website builder',
+    imgSrc: '/static/images/logo.jpg',
+  },
+  {
+    type: 'indonesia',
+    title: 'Pilot - Shopify Hydrogen theme',
+    imgSrc: '/static/images/weaverse-pilot.jpg',
+  },
+  {
+    type: 'work',
+    title: 'Minimog - Shopify theme',
+    imgSrc: '/static/images/minimog.jpg',
+  },
+  {
+    type: 'work',
+    title: 'Fox Kit - Shopify app',
+    imgSrc: '/static/images/foxkit.jpg',
+  },
+  {
+    type: 'self',
+    title: 'Personal website',
+    imgSrc: '/static/images/leoblog.jpg',
+  },
+  {
+    type: 'self',
+    title: 'Shopify theme starter',
+    imgSrc: '/static/images/shopify-theme-store.png',
+  },
+  {
+    type: 'self',
+    title: 'Exercism solutions',
+    imgSrc: '/static/images/exercism.png',
+  },
+  {
+    type: 'self',
+    title: 'Animate loading bar',
+    imgSrc: '/static/images/animate-loading-bar.jpg',
+  },
+  {
+    type: 'self',
+    title: 'Shopify KoaJS React boilerplate',
+    imgSrc: '/static/images/shopify-app-store.png',
+  },
+  {
+    type: 'self',
+    title: 'Travel Egypt Picture Puzzle',
+    imgSrc: '/static/images/travel-egypt.jpg',
+  },
+  {
+    type: 'self',
+    title: 'Infinite Loading Gallery',
+    imgSrc: '/static/images/infinite-gallery.jpg',
+  },
+  {
+    type: 'self',
+    title: 'Store Manager',
+    imgSrc: '/static/images/java-store-manager.jpg',
+  },
+  {
+    type: 'self',
+    title: 'Tiny retry',
+    imgSrc: '/static/images/tiny-retry.png',
+  },
+]
