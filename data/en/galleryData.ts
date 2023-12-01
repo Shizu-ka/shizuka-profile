700238b48be3d96c723a3b6cf147dc31f44e8a71
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-12-02 03:43:00 +0700

    Update galleryData.ts

--- data/en/galleryData.ts
+++ data/en/galleryData.ts
@@ -6,6 +6,6 @@ export let galleryData: Project_g[] = [
     title: 'COVID 19',
     description: `Spreading of covid in Indonesia`,
     imgSrc: '/static/images/dashboard/covid19.png',
-    url: 'http://dashboard.shizuka.my.id/?id=0049e8c6-460c-4102-b343-19d0a04ef58a&title=Spread+of+Covid+in+Indonesia',
+    url: 'https://dashboard.shizuka.my.id/?id=0049e8c6-460c-4102-b343-19d0a04ef58a&title=Spread+of+Covid+in+Indonesia',
   },
 ]
