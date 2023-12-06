7e45bec0cc8d76fe75c5cfd81ab2600726b205b3
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-12-07 01:00:01 +0700

    Update galleryData.ts

--- data/en/galleryData.ts
+++ data/en/galleryData.ts
@@ -3,17 +3,24 @@ import type { Project_g } from '~/types/data'
 export let galleryData: Project_g[] = [
   {
     type: 'self',
-    title: 'COVID 19 Dashboard',
-    description: `Spreading of covid in Indonesia.`,
-    imgSrc: '/static/images/dashboard/covid19.png',
-    url: 'https://dashboard.shizuka.my.id/?id=0049e8c6-460c-4102-b343-19d0a04ef58a&title=Spread+of+Covid+in+Indonesia',
-  },
-  {
-    type: 'self',
     title: 'Customer Feedback Dashboard',
     description: `Costumer feedback data visualization, with prepocessing.`,
     imgSrc: '/static/images/dashboard/CustomerFeedbackDashboard.png',
     url: 'https://dashboard.shizuka.my.id/?id=9f1628d2-b3b2-4dbe-8c23-993a0d47f6b5&title=Customer+Feedback+Dashboard',
+  },
+  {
+    type: 'self',
+    title: 'Trending YouTube Video GB Dashboard',
+    description: `This is a trending youtube video overview dashboard, study case Great Britain.`,
+    imgSrc: '/static/images/dashboard/youtubeTrendingGB.png',
+    url: 'https://dashboard.shizuka.my.id/?id=896a8eac-d199-4d3e-871d-d16a18307556&title=Trending+YouTube+Video+GB+Dashboard',
+  },
+  {
+    type: 'self',
+    title: 'COVID 19 Dashboard',
+    description: `Spreading of covid in Indonesia.`,
+    imgSrc: '/static/images/dashboard/covid19.png',
+    url: 'https://dashboard.shizuka.my.id/?id=0049e8c6-460c-4102-b343-19d0a04ef58a&title=Spread+of+Covid+in+Indonesia',
   },
   {
     type: 'colab',
