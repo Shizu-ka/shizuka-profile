04502ddef47e1f6324ec308dc36d0c66c608f47f
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2024-09-13 13:00:42 +0700

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ export let headerNavLinks = [
   { href: '/blog', label: 'menu_blog' },
   // { href: '/snippets', label: 'menu_snippets' },
   { href: '/projects', label: 'menu_projects' },
-  { href: '/about', label: 'menu_about' },
-  { href: 'https://github.com/Shizu-ka', label: 'menu_resume' },
   { href: '/dashboard', label: 'menu_dashboard' },
+  { href: 'https://github.com/Shizu-ka', label: 'menu_resume' },
+  { href: '/about', label: 'menu_about' },
 ]
